65945327f225c15ff6b01481fa860161ff96b0a9
Tomi Tavela <tomi.tavela@gmail.com>
2014-08-01 02:25:59 +0300

    lua server pages location

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,2 +1,9 @@
 local template = require "resty.template"
-template.render(ngx.var.pathtoluapage .. ".html")
+
+local f=io.open(ngx.var.template_root .. "/" .. ngx.var.pathtoluapage .. ".html","r")
+if f~=nil then
+  io.close(f)
+  template.render(ngx.var.pathtoluapage .. ".html")
+else
+  ngx.say("page not found")
+end
